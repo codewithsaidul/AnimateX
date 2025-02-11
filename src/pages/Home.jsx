faa2d9040a460ce8ee00250ed20b85ae2582c8a3
codewithsaidul <codewithsaidul@gmail.com>
2025-02-11 21:46:01 +0600

    Category added on Sidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,19 @@
 import Fetured from "../components/Home/Fetured"
+import Genre from "../components/Home/Genre"
 import Hero from "../components/Home/Hero"
 
 
 const Home = () => {
   return (
-    <div className="pt-16">
-      <div className="w-full">
+    <div className="pt-16 flex gap-6">
+      <div className="w-[75%]">
         <Hero />
         <Fetured />
       </div>
 
-      {/* <div className="min-h-screen w-[20%] bg-rose-400"></div> */}
+      <div className="min-h-screen w-[25%]">
+        <Genre />
+      </div>
     </div>
   )
 }
