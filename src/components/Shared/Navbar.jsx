a184b21e328f58821e49b8fdeee9016657300f2a
codewithsaidul <codewithsaidul@gmail.com>
2025-02-21 01:53:22 +0600

    nav menu hidden on mobile device

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+ import { useState } from "react";
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { useNavigate } from "react-router-dom";
 
@@ -68,6 +68,7 @@ const Navbar = () => {
             <FaMagnifyingGlass size={24} />
           </button>
         </form>
+
       </nav>
     </header>
   );
